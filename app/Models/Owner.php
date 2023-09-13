85f18dc10c700516f984b261aa776b9b8af6bb4a
Kazumean <kazumin.angelav@gmail.com>
2023-09-13 16:57:47 +0900

    OwnerテーブルとShopテーブルのリレーションを設定。

--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Shop;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Owner extends Authenticatable
 {
@@ -40,4 +41,9 @@ class Owner extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function shop()
+    {
+        return $this->hasOne(Shop::class);
+    }
 }
--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Owner;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Shop extends Model
 {
     use HasFactory;
+
+    public function owner()
+    {
+        return $this->belongsTo(Owner::class);
+    }
 }
